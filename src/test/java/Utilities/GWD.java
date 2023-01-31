c0e8f651003358ec6e6dc6c06a27f5425048459d
serkanXman <msozkilic@gmail.com>
2023-01-31 11:38:17 +0100

    GENEL WEB DRIVER

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -4,53 +4,79 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.slf4j.impl.SimpleLogger;
 
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static Utilities.GWDBasic.Bekle;
+
 public class GWD {
-    private static WebDriver driver;
+
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();//WebDriver1,WebDriver 2
+    public static ThreadLocal<String> threadBrowserName = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
+        Locale.setDefault(new Locale("EN"));//todo dille cikan problemleri hallediyor
+        System.setProperty("user.language", "EN");
+        Logger.getLogger("").setLevel(Level.SEVERE);
+        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "Error");
 
-        if (driver == null) {
-            Logger.getLogger("").setLevel(Level.SEVERE);
-            System.setProperty(ChromeDriverService.CHROME_DRIVER_APPEND_LOG_PROPERTY, "true");
-            System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "Error");
+        if(threadBrowserName.get()==null)//paralel calismayan yani xmlden parametreyle gelmeyen
+            threadBrowserName.set("chrome");
 
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+        if (threadDriver.get() == null) {
 
-            //todo firefox
-            // WebDriverManager.firefoxdriver().setup();
-            //driver=new FirefoxDriver();
+            String browserName = threadBrowserName.get();
+            switch (browserName) {
+                case "chrome":
+                    System.setProperty(ChromeDriverService.CHROME_DRIVER_APPEND_LOG_PROPERTY, "true");
+                    WebDriverManager.chromedriver().setup();
+                    threadDriver.set(new ChromeDriver());
+                    break;
+                case "firefox":
+                    System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+                    WebDriverManager.chromedriver().setup();
+                    threadDriver.set(new FirefoxDriver());
+                case "safari":
+
+                    WebDriverManager.safaridriver().setup();
+                    threadDriver.set(new SafariDriver());
+                case "edge":
+
+                    WebDriverManager.edgedriver().setup();
+                    threadDriver.set(new EdgeDriver());
+                    break;
+            }
+            return threadDriver.get();
         }
-        return driver;
-
-    }
 
     public static void quitDriver() {
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        if (driver != null)
-            driver.quit();
-        driver = null;
-    }
+            Bekle(5);
 
-    public static void Bekle(int saniye) {
+            if (threadDriver.get() != null) {
+                threadDriver.get().quit();
 
-        try {
-            Thread.sleep(saniye+5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-
+                WebDriver driver = threadDriver.get();
+                driver = null;
+                threadDriver.set(driver);
+            }
         }
 
+        public static void Bekle(int saniye){
+
+            try {
+                Thread.sleep(saniye + 5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+
+            }
+
+        }
     }
 }
