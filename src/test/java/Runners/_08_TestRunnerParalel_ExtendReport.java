26c140191bfdfea57129886b51bb303282947d58
serkanXman <msozkilic@gmail.com>
2023-02-09 09:49:37 +0100

    paralel extend report

---
+++ src/test/java/Runners/_08_TestRunnerParalel_ExtendReport.java
@@ -0,0 +1,26 @@
+package Runners;
+
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+
+@CucumberOptions(
+        tags = "@Regression",
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"},
+        plugin = {"html:target//cucumber-reports.html"}
+)
+public class _08_TestRunnerParalel_ExtendReport extends AbstractTestNGCucumberTests {
+
+
+    @AfterClass
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("User Name","Serkan Kilic");
+        ExtentService.getInstance().setSystemInfo("Application Name","Campus");
+        //ExtentService.getInstance().setSystemInfo("Operating System Info",System.getProperty());
+        ExtentService.getInstance().setSystemInfo("Department","QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satir","Aciklama");
+    }
+
+}
