df68c399a9a785746a6acc5ec9ff1671bff3db96
serkanXman <msozkilic@gmail.com>
2023-02-14 11:33:01 +0100

    ApachiPoi

---
+++ src/test/java/ApachiPOI/_03_ApachiPOIStart.java
@@ -0,0 +1,27 @@
+package ApachiPOI;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class _03_ApachiPOIStart {
+public static void main(String[]args) throws IOException {
+        String path="src/test/java/ApachiPOI/resource/ApacheExcel2.xlsx";
+        FileInputStream dosyaOkumaBaglantisi=new FileInputStream(path);
+
+        Workbook calismaKitabi= WorkbookFactory.create(dosyaOkumaBaglantisi);
+
+        Sheet calismaSayfasi=calismaKitabi.getSheet("Sayfa1");
+
+        Row satir=calismaSayfasi.getRow(0);
+
+        Cell hücre= satir.getCell(0);
+
+        System.out.println("hücre ="+ hücre);
+
+        }
+}
