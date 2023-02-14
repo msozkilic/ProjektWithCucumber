8aa3ad7a5780da6c2a50bfecce2925e9c70cee2a
serkanXman <msozkilic@gmail.com>
2023-02-14 11:34:11 +0100

    ApachiPoiGet

---
+++ src/test/java/ApachiPOI/_05_GetASpecificData.java
@@ -0,0 +1,7 @@
+package ApachiPOI;
+
+public class _05_GetASpecificData {
+    //verilen exceldeki password bilgisini bir metod araciligi bularak yazdiriniz
+    //yani metoda password kelimesi gönderilecek,konen deger password un kendisi olacka
+    //src/test/java/ApachePOI/resources/LoginData.xlsx
+}
