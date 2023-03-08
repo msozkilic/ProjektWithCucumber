4fc3b8e0165b1202ce3023840dc02c9f101f617d
serkanXman <msozkilic@gmail.com>
2023-03-08 08:50:54 +0100

    soru

---
+++ src/test/java/_JDBC/Gün1/Soru.java
@@ -0,0 +1,22 @@
+package _JDBC.Gün1;
+
+import _JDBC.Gün2.JDBCParent;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+public class Soru extends JDBCParent {
+    //todo city tablosundaki tüm kayitlari next ile yapiniz
+    @Test
+    public void test() throws SQLException {
+        ResultSet rs=statement.executeQuery("select * from city");
+
+        while(rs.next()){
+            String cityName=rs.getString(2);
+            System.out.println("cityName ="+cityName);
+        }
+
+
+
+    }
+}
