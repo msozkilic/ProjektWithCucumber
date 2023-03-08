2557ffd4e6cd03d843825e17d3133577e75e0004
serkanXman <msozkilic@gmail.com>
2023-03-08 19:50:49 +0100

    antwort

---
+++ src/test/java/_JDBC/Gün2/SoruCevabi.java
@@ -0,0 +1,39 @@
+package _JDBC.Gün2;
+
+public class SoruCevabi {
+    /*
+    String myDriver = "org.gjt.mm.mysql.Driver";
+    String myUrl = "jdbc:mysql://localhost/test";
+    Class.forName(myDriver);
+    Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+
+    String sql = " insert into users (first_name, last_name, date_created, is_admin, num_points)"
+            + " values (?, ?, ?, ?, ?)";
+
+
+    PreparedStatement preparedStmt = conn.prepareStatement(sql);
+  preparedStmt.setString (1, s.first_name);
+  preparedStmt.setString (2, s.last_name);
+  preparedStmt.setDate   (3, s.date_created);
+  preparedStmt.setBoolean(4, s.is_admin);
+  preparedStmt.setInt    (5, s.num_points);
+
+
+  preparedStmt.execute();
+
+
+  conn.close();
+
+
+  catch (Exception e)
+    {
+        System.err.println("Got an exception!");
+        // printStackTrace method
+        // prints line numbers + call stack
+        e.printStackTrace();
+        // Prints what exception has been thrown
+        System.out.println(e);
+    }
+*/
+}
