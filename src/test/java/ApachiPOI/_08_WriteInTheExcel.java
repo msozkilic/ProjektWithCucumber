da8ee53a720768c365635ec3df32f8bfde74db09
serkanXman <msozkilic@gmail.com>
2023-05-17 19:06:17 +0200

    write excel 2

--- src/test/java/ApachiPOI/_08_WriteInTheExcel.java
+++ src/test/java/ApachiPOI/_08_WriteInTheExcel.java
@@ -28,5 +28,14 @@ public class _08_WriteInTheExcel {
         //todo sira kaydetmeye geldi ,bütün bilgile hafizada buraya kadar.
         inputStream.close();
 
+        //todo Yazma islemini YAZMA modunda acip öyle yapacagiz.
+        FileOutputStream outputStream=new FileOutputStream(path);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+        System.out.println("islem tamamlandi.");
+
+    }
+
 
 }
