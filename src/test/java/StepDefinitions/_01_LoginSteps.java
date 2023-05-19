66988743e8120096366c757598a1be32fe011d7b
serkanXman <msozkilic@gmail.com>
2023-05-19 22:59:45 +0200

    loginstesp3

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -28,5 +28,13 @@ public class _01_LoginSteps extends Parent {
         dc.findAndSend("password","TechnoStudy123");
         dc.findAndClick("loginButton");}
 
+
+    @Then("User should login succesfully")
+    public void userShouldLoginSuccessfuly(){
+        dc.findAndContainsText("dashboard","Dashboard");
+
+
+
+    }
 }
 
