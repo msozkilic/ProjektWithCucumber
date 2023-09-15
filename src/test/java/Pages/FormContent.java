89aed85c3c8961f8f5a9d3bd61acc26b629395c7
serkanXman <msozkilic@gmail.com>
2023-09-15 09:21:04 +0200

    FormContent

--- src/test/java/Pages/FormContent.java
+++ src/test/java/Pages/FormContent.java
@@ -24,42 +24,6 @@ public class FormContent extends Parent{
     private WebElement gradeLevel2;
 
     public WebElement myElement;
-    public void findAndSend(String strElement,String value){
-
-        switch (strElement){
-        }
-        sendKeysFunction(myElement,value);
-    }
-    public void findAndClick(String strElement){
-
-        switch (strElement){
-
-            case "academicPeriod" :myElement=academicPeriod; break;
-            case "academicPeriod1" :myElement=academicPeriod1; break;
-            case "gradeLevel" :myElement=gradeLevel; break;
-            case "gradeLevel2" :myElement=gradeLevel2; break;
-
-        }
-        clickFunction(myElement);
-    }
-    public void findAndContainsText(String strElement,String text){
-
-        switch (strElement){
-           // case "dashboard" :myElement=dashboard; break;
-
-
-        }
-        verifyContainsText(myElement,text);
-    }
-    public void searchAndDelete(String searchtText){
-        //todo arama kutucuguna kelimeyi yaz,arama butonuna bas,silme butonuna bas,diaologdaki silme but bas.,,
-        findAndSend(searchtText,"searchText");
-        findAndClick("searchButton");
-
-        GWD.Bekle(2);
-        findAndClick("deleteButton");
-        findAndClick("deleteDialogBtn");
-    }
 
 
 }
