60906c7ef19440076de2e6e9ed1f321a5ec56818
serkanXman <msozkilic@gmail.com>
2023-09-22 10:35:25 +0200

    country

--- src/test/java/StepDefinitions/_02_CountrySteps.java
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -17,26 +17,6 @@ public class _02_CountrySteps {
         ln.findAndClick("parameters");
         ln.findAndClick("countries");
     }
-    @When("Create a country")
-    public void create_a_country() {
-        String randomGenName= RandomStringUtils.randomAlphabetic(8);
-        String randomGenCode=RandomStringUtils.randomAlphabetic(4);
-        dc.findAndClick("addButton");
-        dc.findAndSend("nameInput", randomGenName);
-        dc.findAndSend("codeInput", randomGenCode);
-        dc.findAndClick("saveButton");
-    }
 
-    @Then("Success message should be displayed")
-    public void success_message_should_be_displayed() {
-        dc.findAndContainsText("successMessage", "success");
-    }
-    @When("Create a country name as {string} code as {string}")
-    public void create_a_country_name_as_code_as(String name, String code) {
-        dc.findAndClick("addButton");
-        dc.findAndSend("nameInput", name);
-        dc.findAndSend("codeInput", code);
-        dc.findAndClick("saveButton");
-    }
 
 }
