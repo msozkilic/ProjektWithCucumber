bbc2279ab4fb1ecee73370ca81eb9eadfe91b1da
serkanXman <msozkilic@gmail.com>
2023-10-18 08:50:45 +0200

    data

--- src/test/java/StepDefinitions/_05_DataTableSteps.java
+++ src/test/java/StepDefinitions/_05_DataTableSteps.java
@@ -55,15 +55,7 @@ public class _05_DataTableSteps {
             dc.searchAndDelete(listElement.get(i));
         }}
 
-    @And("Click on the element in the Form Content")
-    public void clickOnTheElementInTheFormContent(DataTable elements) {
-            List<String> listElement = elements.asList(String.class);
 
-            for (int i = 0; i < listElement.size(); i++) {
-                fc.findAndClick(listElement.get(i));
-
-            }
-    }
 
 
 }
