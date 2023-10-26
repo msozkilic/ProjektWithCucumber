3ab4eea3ff6552d51a015474be49051a7ec2a796
serkanXman <msozkilic@gmail.com>
2023-10-26 10:36:33 +0200

    features

--- src/test/java/Runners/_02_TestRunnerBelirliFeaturler.java
+++ src/test/java/Runners/_02_TestRunnerBelirliFeaturler.java
@@ -4,6 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+        features = {"src/test/java/FeatureFiles/_01_Login.feature",
+                "src/test/java/FeatureFiles/_02_Country.feature"}
 
 )
 public class _02_TestRunnerBelirliFeaturler extends AbstractTestNGCucumberTests {
