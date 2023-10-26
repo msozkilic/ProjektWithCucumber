9ce924a7314ee34ee8812f7e189a87df0f27cb44
serkanXman <msozkilic@gmail.com>
2023-10-26 10:35:51 +0200

    options

--- src/test/java/Runners/_02_TestRunnerBelirliFeaturler.java
+++ src/test/java/Runners/_02_TestRunnerBelirliFeaturler.java
@@ -3,6 +3,8 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(
 
+)
 public class _02_TestRunnerBelirliFeaturler extends AbstractTestNGCucumberTests {
 }
