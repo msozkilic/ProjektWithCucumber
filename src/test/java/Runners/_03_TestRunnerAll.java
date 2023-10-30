9fb0e6349143a5c7730da4ba1020024c0ed05fce
serkanXman <msozkilic@gmail.com>
2023-10-30 09:00:15 +0100

    features

--- src/test/java/Runners/_03_TestRunnerAll.java
+++ src/test/java/Runners/_03_TestRunnerAll.java
@@ -3,6 +3,7 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
+        features = {"src/test/java/FeatureFiles"},
 
 )
 
