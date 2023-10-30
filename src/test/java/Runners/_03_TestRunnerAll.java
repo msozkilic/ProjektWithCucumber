3df97df0af06bcbcae0b65ffdddcee89384ae432
serkanXman <msozkilic@gmail.com>
2023-10-30 08:59:51 +0100

    options

--- src/test/java/Runners/_03_TestRunnerAll.java
+++ src/test/java/Runners/_03_TestRunnerAll.java
@@ -2,6 +2,9 @@ package Runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(
+
+)
 
 public class _03_TestRunnerAll extends AbstractTestNGCucumberTests {
 }
