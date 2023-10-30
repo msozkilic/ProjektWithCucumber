4636b7b76c1e35a8f3544469471edc9ad40dceb8
serkanXman <msozkilic@gmail.com>
2023-10-30 09:00:34 +0100

    glue

--- src/test/java/Runners/_03_TestRunnerAll.java
+++ src/test/java/Runners/_03_TestRunnerAll.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles"},
-
+        glue = {"StepDefinitions"}
 )
 
 public class _03_TestRunnerAll extends AbstractTestNGCucumberTests {
