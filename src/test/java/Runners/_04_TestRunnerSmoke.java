30e0e8560d16950002c8e7f237388fb36f6fe2cc
serkanXman <msozkilic@gmail.com>
2023-11-01 10:39:04 +0100

    features

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -4,7 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@SmokeTest"
+        tags = "@SmokeTest",
+        features = {"src/test/java/FeatureFiles"}
+
 
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
