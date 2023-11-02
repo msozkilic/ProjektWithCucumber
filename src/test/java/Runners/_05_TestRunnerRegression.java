8e4606bb5c8334e05265d3a822db4e7569823c8d
serkanXman <msozkilic@gmail.com>
2023-11-02 09:45:27 +0100

    options

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -4,6 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
+@CucumberOptions(
 
+)
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
