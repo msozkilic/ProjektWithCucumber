022e041275247df733943133b41fe241055d2a38
serkanXman <msozkilic@gmail.com>
2023-11-06 10:31:42 +0100

    tags

--- src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
@@ -3,6 +3,8 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
+@CucumberOptions(
+        tags = "@SmokeTest"
+)
 public class _06_TestRunnerRegressionWithPlugin extends AbstractTestNGCucumberTests {
 }
