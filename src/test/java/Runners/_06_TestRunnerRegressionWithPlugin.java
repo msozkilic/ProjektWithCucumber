b32f51bf1578b105438cd020e55f7be7d9a691d6
serkanXman <msozkilic@gmail.com>
2023-11-06 10:31:02 +0100

    glue

--- src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
@@ -3,11 +3,6 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-        tags = "@SmokeTest",
-        features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"},
-        plugin = {"html:target//cucumber-reports.html"}
-)
+
 public class _06_TestRunnerRegressionWithPlugin extends AbstractTestNGCucumberTests {
 }
