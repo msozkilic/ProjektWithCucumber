3aaef347031593d19ffb064d20d674ca6d4c8217
serkanXman <msozkilic@gmail.com>
2023-11-13 11:25:05 +0100

    features

--- src/test/java/Runners/_08_TesrRunnerParalel.java
+++ src/test/java/Runners/_08_TesrRunnerParalel.java
@@ -6,7 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+@CucumberOptions(
 
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"},
+        plugin = {"html:target//cucumber-reports.html"}
+)
 public class _08_TesrRunnerParalel extends AbstractTestNGCucumberTests {
 
 
