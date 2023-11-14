28dcd5978857433b011fccf8ae7b233a4eab5365
serkanXman <msozkilic@gmail.com>
2023-11-14 12:17:09 +0100

    extend

--- src/test/java/Runners/_08_TestRunnerParalel_ExtendReport.java
+++ src/test/java/Runners/_08_TestRunnerParalel_ExtendReport.java
@@ -14,13 +14,6 @@ import org.testng.annotations.AfterClass;
 public class _08_TestRunnerParalel_ExtendReport extends AbstractTestNGCucumberTests {
 
 
-    @AfterClass
-    public static void writeExtentReport() {
-        ExtentService.getInstance().setSystemInfo("User Name","Serkan Kilic");
-        ExtentService.getInstance().setSystemInfo("Application Name","Campus");
-        //ExtentService.getInstance().setSystemInfo("Operating System Info",System.getProperty());
-        ExtentService.getInstance().setSystemInfo("Department","QA");
-        ExtentService.getInstance().setSystemInfo("Ek Satir","Aciklama");
-    }
+
 
 }
