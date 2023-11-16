2cf3c9a205f72af1f878c72f2a95e93c0a17554e
serkanXman <msozkilic@gmail.com>
2023-11-16 17:32:46 +0100

    paralelMaster

--- src/test/java/Runners/_08_TestRunnerParalel_MasterReport.java
+++ src/test/java/Runners/_08_TestRunnerParalel_MasterReport.java
@@ -15,13 +15,5 @@ import org.testng.annotations.Parameters;
 )
 public class _08_TestRunnerParalel_MasterReport extends AbstractTestNGCucumberTests {
 
-    @BeforeClass(alwaysRun = true) // bazı java versiyon hatalırı için
-    @Parameters("browser")
-    public void beforeClass(String browser)
-    {
-        GWD.threadBrowserName.set(browser);
-        //burada browser set edilecek
-        // bu threade browsername set edildi.
-    }
 
 }
