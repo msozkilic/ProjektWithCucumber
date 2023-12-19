f74a6db17d0dd328ccfc719b8dd0ed88733f2d57
serkanXman <msozkilic@gmail.com>
2023-12-19 14:38:24 +0100

    zoo

--- src/test/java/ApachiPOI/_01_2DArray.java
+++ src/test/java/ApachiPOI/_01_2DArray.java
@@ -2,9 +2,13 @@ package ApachiPOI;
 
 public class _01_2DArray {
     public static void main(String[] args) {
-        String[][] zoo={{"Aslan","1"},{"Kaplan","2"},{"Zebra","3","1"},{"Fil","4"}};
+        String[][] zoo = {{"Aslan", "1"}, {"Kaplan", "2"}, {"Zebra", "3", "1"}, {"Fil", "4"}};
+        System.out.println("------------------------------------------");
+        System.out.println(zoo[2][0]);
+
 
 
         }
     }
+
 
