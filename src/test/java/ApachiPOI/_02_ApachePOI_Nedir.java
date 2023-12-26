c9ad8377197d60499455ac2276d31a666dd2bf23
serkanXman <msozkilic@gmail.com>
2023-12-26 17:35:08 +0100

    apachePoi

--- src/test/java/ApachiPOI/_02_ApachePOI_Nedir.java
+++ src/test/java/ApachiPOI/_02_ApachePOI_Nedir.java
@@ -3,5 +3,6 @@ package ApachiPOI;
 public class _02_ApachePOI_Nedir {
     //todo Interview Sorusu 1-Excelden veri okumasini nasil yaparsin? Apachi.POI ile.
 //todo ApachiPoi nedir? Excelden veri okuma araci,kütüphanesi.
-
-}
+//todo ApachiPOi yi nasil kullaniyorsun?
+//todo FileInputStream olusturuyorum.(Dosyayi yani Exceli okumak icin
+//todo FileInputStream -> Workbook -> Sheet -> row -> cell iliski sirasina göre
